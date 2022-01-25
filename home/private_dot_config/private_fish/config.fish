87034a63bdd575becbde9fbe5fa3e3160c3426ce
cjw6k <chris@lot2k.com>
2022-01-24 23:21:43 -0400

    home bin on path

--- home/private_dot_config/private_fish/config.fish
+++ home/private_dot_config/private_fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     set fish_greeting
     set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+    set -x PATH "$PATH:$HOME/bin"
     starship init fish | source
     cd ~
 end
