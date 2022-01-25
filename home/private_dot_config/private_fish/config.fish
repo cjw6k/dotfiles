2ef8a5cff543e9f1555328e23e69d4d4af24c464
cjw6k <chris@lot2k.com>
2022-01-24 23:10:43 -0400

    Revert "home bin on path"
    This reverts commit ffab8f1a40e6bde42d0e60c5edfb112962e71de4.

--- home/private_dot_config/private_fish/config.fish
+++ home/private_dot_config/private_fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
     set fish_greeting
     set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
-    set -x PATH $PATH:~/bin
     starship init fish | source
     cd ~
 end
