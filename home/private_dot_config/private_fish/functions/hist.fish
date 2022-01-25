57d0c7e1bc586efb67b7f4ad52fcbb4a2601bf49
cjw6k <chris@lot2k.com>
2022-01-25 02:37:42 -0400

    fish: make hist more portable

--- home/private_dot_config/private_fish/functions/hist.fish
+++ home/private_dot_config/private_fish/functions/hist.fish
@@ -1,3 +1,6 @@
 function hist
+        if test -z $PAGER
+                set --local PAGER (which pager)
+        end
         history --show-time=(tput setaf 235)"[%Y-%m-%d %H:%M:%S] "(tput sgr0) $argv | $PAGER
 end
