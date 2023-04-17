bf01185576e13b0bafc2f98e68a27167b5057ec4
cjw6k <cj@w6k.ca>
2023-04-17 20:14:16 -0300

    freebsd utils

--- home/.chezmoiscripts/linux/debian/run_onchange_before_install-jq.sh
+++ home/.chezmoiscripts/linux/debian/run_onchange_before_install-jq.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-# ~/.config/dotfiles/utils.json hash:
-# {{ include "private_dot_config/dotfiles/utils.json" | sha256sum }}
-
 sudo apt-get install -y jq
--- home/.chezmoiscripts/linux/gentoo/run_onchange_before_install-jq.sh
+++ home/.chezmoiscripts/linux/gentoo/run_onchange_before_install-jq.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env sh
 
-# ~/.config/dotfiles/utils.json hash:
-# {{ include "private_dot_config/dotfiles/utils.json" | sha256sum }}
-
 emerge -n app-misc/jq
