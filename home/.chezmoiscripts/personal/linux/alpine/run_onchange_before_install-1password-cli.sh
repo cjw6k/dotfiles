d81ac000ac891c1105e43dacc9b44211f9329563
cjw6k <cj@w6k.ca>
2023-08-19 14:09:27 -0300

    use sudo

--- home/.chezmoiscripts/personal/linux/alpine/run_onchange_before_install-1password-cli.sh
+++ home/.chezmoiscripts/personal/linux/alpine/run_onchange_before_install-1password-cli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-echo https://downloads.1password.com/linux/alpinelinux/stable/ >> /etc/apk/repositories
-wget https://downloads.1password.com/linux/keys/alpinelinux/support@1password.com-61ddfc31.rsa.pub -P /etc/apk/keys
-apk update && apk add 1password-cli
+echo https://downloads.1password.com/linux/alpinelinux/stable/ | sudo tee -a /etc/apk/repositories
+sudo wget https://downloads.1password.com/linux/keys/alpinelinux/support@1password.com-61ddfc31.rsa.pub -P /etc/apk/keys
+sudo apk update && sudo apk add 1password-cli
