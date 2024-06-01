01178ee20512e70a8216089940e5143387800231
cjw6k <cj@w6k.ca>
2024-06-01 17:14:31 -0300

    test

--- home/lib.sh
+++ home/lib.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env sh
 
-lib_test() {
-  echo "$1"
+exit_with_error() {
+	echo "lib.sh: $1"
+	exit 1
+}
+
+standard_utils() {
+  script="
+    .[]
+    | .install.linux.$1.\"$2\"
+      // .installs.linux.alpine.common
+      // .installs.linux.common
+      // .installs.common
+      // .name
+    | select(. as \$in | ['#builtin', '#script', '#na'] | index(\$in) | not)
+  "
+
+  utils=$(jq -r "$script" ~/.config/dotfiles/utils/standard.json)
 }
