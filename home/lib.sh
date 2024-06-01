a77bdc65db52cd11d334b39e179d6e28f08f2583
cjw6k <cj@w6k.ca>
2024-06-01 19:25:48 -0300

    refactor

--- home/lib.sh
+++ home/lib.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env sh
 
-exit_with_error() {
-	echo "lib.sh: $1"
-	exit 1
+build_jq_filter() {
+  filter=".[] |"
+  if test -n "$3"; then
+    filter="$filter .installs.$1.$2.\"$3\" // .installs.$1.$2.common //"
+  else
+    filter="$filter .installs.$1.$2 //"
+  fi
+
+  filter="$filter .installs.$1.common // .installs.common // .name"
+  filter="$filter | select(. as \$in | [\"#builtin\", \"#script\", \"#na\"] | index(\$in) | not)"
+}
+
+run_jq_filter() {
+    filter "$2" "$3" "$4"
+    utils=$(jq -r "$filter" "$HOME/.config/dotfiles/utils/$1.json")
 }
 
 standard_utils() {
-  script="
-    .[]
-    | .install.linux.$1.\"$2\"
-      // .installs.linux.$1.common
-      // .installs.linux.common
-      // .installs.common
-      // .name
-    | select(. as \$in | [\"#builtin\", \"#script\", \"#na\"] | index(\$in) | not)
-  "
-
-  utils=$(jq -r "$script" ~/.config/dotfiles/utils/standard.json)
+  run_jq_filter standard "$1" "$2" "$3"
 }
