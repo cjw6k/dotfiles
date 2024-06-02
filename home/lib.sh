543ab4aace63190ab690436551eabe6c1b4fa4ce
cjw6k <cj@w6k.ca>
2024-06-02 15:46:07 -0300

    alternative pip utils install

--- home/lib.sh
+++ home/lib.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env sh
 
-installation_sources_filter() {
-  filter=".[][] |"
+source_filter() {
+  sourceFilter=""
   if test -n "$3"; then
-    filter="$filter .sources.$1.$2.\"$3\" // .sources.$1.$2.common //"
+    sourceFilter="$sourceFilter .sources.$1.$2.\"$3\" // .sources.$1.$2.common //"
   else
-    filter="$filter .sources.$1.$2 //"
+    sourceFilter="$sourceFilter .sources.$1.$2 //"
   fi
 
-  filter="$filter .sources.$1.common // .sources.common // .name"
+  sourceFilter="$sourceFilter .sources.$1.common // .sources.common // .name"
+}
+
+installation_sources_filter() {
+
 }
 
 filter_utils() {
-  installation_sources_filter "$3" "$4" "$5"
+  source_filter "$3" "$4" "$5"
   # allow globbing
   # shellcheck disable=SC2086
-  utils=$(jq -rs "$filter $2" ~/.config/dotfiles/utils/$1.json)
+  utils=$(jq -rs ".[][] | $sourceFilter $2" ~/.config/dotfiles/utils/$1.json)
 }
 
 standard_utils() {
@@ -27,5 +31,8 @@ dev_utils() {
 }
 
 pip_utils() {
-  filter_utils "dev/*" "| select(. == \"#pip\")" "$1" "$2" "$3"
+  source_filter "$1" "$2" "$3"
+  # allow globbing
+  # shellcheck disable=SC2086
+  utils=$(jq -rs ".[][] | select($sourceFilter) == \"#pip\" | .name" ~/.config/dotfiles/utils/dev/*.json)
 }
