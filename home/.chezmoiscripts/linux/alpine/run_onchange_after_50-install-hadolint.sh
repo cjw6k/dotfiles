fb6714059e589fca776df5a945d094b02a0b0639
cjw6k <cj@w6k.ca>
2024-06-02 18:23:24 -0300

    fixup

--- home/.chezmoiscripts/linux/alpine/run_onchange_after_50-install-hadolint.sh
+++ home/.chezmoiscripts/linux/alpine/run_onchange_after_50-install-hadolint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 curl -sLO https://github.com/hadolint/hadolint/releases/download/v2.12.0/hadolint-Linux-x86_64 \
-  && mv ./hadolint-Linux-x86_64 ~/.local/bin
+  && mv ./hadolint-Linux-x86_64 ~/.local/bin/hadolint
