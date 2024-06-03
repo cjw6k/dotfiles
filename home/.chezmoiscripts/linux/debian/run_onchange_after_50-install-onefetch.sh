96c6ae0164c6b1cb285b89cf1f404733461034db
cjw6k <cj@w6k.ca>
2024-06-02 23:41:44 -0300

    fix various dev utils on debian

---
+++ home/.chezmoiscripts/linux/debian/run_onchange_after_50-install-onefetch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+curl -sLO https://github.com/o2sh/onefetch/releases/download/2.21.0/onefetch-linux.tar.gz \
+  && tar xzf onefetch-linux.tar.gz onefetch \
+  && mv ./onefetch ~/.local/bin \
+  && rm onefetch-linux.tar.gz
