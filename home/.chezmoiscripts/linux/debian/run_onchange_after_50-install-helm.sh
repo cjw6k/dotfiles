48a31675a1627d6ab2e7451c29bd6bb0d61a1abe
cjw6k <cj@w6k.ca>
2024-06-02 23:44:20 -0300

    fix helm install on debian

--- home/.chezmoiscripts/linux/debian/run_onchange_after_50-install-helm.sh
+++ home/.chezmoiscripts/linux/debian/run_onchange_after_50-install-helm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sh -c "$(curl -sL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3)"
+bash -c "$(curl -sL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3)"
