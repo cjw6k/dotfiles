b2f0c721f98c3957f6f7b2ebf803e2ce50fcffdc
cjw6k <cj@w6k.ca>
2024-06-05 20:21:00 -0300

    gentoo: fix hexyl install script

--- home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install-hexyl.sh
+++ home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install-hexyl.sh
@@ -3,5 +3,5 @@
 curl -sLO https://github.com/sharkdp/hexyl/releases/download/v0.14.0/hexyl-v0.14.0-x86_64-unknown-linux-gnu.tar.gz \
   && tar xzf hexyl-v0.14.0-x86_64-unknown-linux-gnu.tar.gz hexyl-v0.14.0-x86_64-unknown-linux-gnu/hexyl \
   && mv hexyl-v0.14.0-x86_64-unknown-linux-gnu/hexyl ~/.local/bin \
-  && rm hexyl-v0.14.0-x86_64-unknown-linux-gnu \
+  && rm -r hexyl-v0.14.0-x86_64-unknown-linux-gnu \
   && rm hexyl-v0.14.0-x86_64-unknown-linux-gnu.tar.gz
