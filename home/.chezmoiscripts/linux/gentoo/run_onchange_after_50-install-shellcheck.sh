2f2e714202242b2bbefbbafd0291b75b76b745f5
cjw6k <cj@w6k.ca>
2024-06-05 20:36:15 -0300

    gentoo: fix shellcheck install

---
+++ home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install-shellcheck.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if [ ! -f /etc/portage/package.accepted_keyworks/dev-util/shellcheck ]; then
+  if [ ! -d /etc/portage/package.accepted_keyworks/dev-util ]; then
+    sudo mkdir -p /etc/portage/package.accepted_keyworks/dev-util
+  fi
+
+  echo 'dev-util/shellcheck-0.9.0-r2 ~amd64' | sudo tee -a /etc/portage/package.accepted_keyworks/dev-util/shellcheck
+fi
+
+sudo emerge -n dev-util/shellcheck
