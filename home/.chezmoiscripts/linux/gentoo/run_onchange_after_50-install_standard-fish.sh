367934f84cf176de763ee7fc7e0c21512068243a
cjw6k <cj@w6k.ca>
2024-06-08 13:53:12 -0300

    gentoo: fix fish install script; use 3.7.1 (test)

--- home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install_standard-fish.sh
+++ home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install_standard-fish.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-if [ ! -f /etc/portage/package.use/app-shells/fish ]; then
-  if [ ! -d /etc/portage/package.use/app-shells ]; then
-    sudo mkdir -p /etc/portage/package.use/app-shells
+if [ ! -f /etc/portage/package.accept_keywords/app-shells/fish ]; then
+  if [ ! -d /etc/portage/package.accept_keywords/app-shells ]; then
+    sudo mkdir -p /etc/portage/package.accept_keywords/app-shells
   fi
 
-  echo '>=dev-libs/libpcre2-10.42-r1 pcre32' | sudo tee /etc/portage/package.use/app-shells/fish
+  echo '=app-shells/fish-3.7.1' | sudo tee /etc/portage/package.accept_keys/app-shells/fish
 fi
 
 sudo emerge -n app-shells/fish
