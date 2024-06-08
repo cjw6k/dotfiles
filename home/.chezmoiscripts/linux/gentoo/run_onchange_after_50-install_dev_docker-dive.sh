2812c64b4e1c952c717866c5a4687122e9c13625
cjw6k <cj@w6k.ca>
2024-06-07 23:00:36 -0300

    gentoo: dev: docker: fix dive install script

--- home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install_dev_docker-dive.sh
+++ home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install_dev_docker-dive.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-curl -sLO https://github.com/wagoodman/dive/releases/download/v0.12.0/dive_0.12.0_linux_amd64.deb \
-  && sudo dpkg -i dive_0.12.0_linux_amd64.deb \
-  && rm dive_0.12.0_linux_amd64.deb
+curl -sLO https://github.com/wagoodman/dive/releases/download/v0.12.0/dive_0.12.0_linux_amd64.tar.gz \
+  && tar xzf dive_0.12.0_linux_amd64.tar.gz dive \
+  && mv ./dive ~/.local/bin \
+  && rm dive_0.12.0_linux_amd64.tar.gz
