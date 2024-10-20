7d8fdcb1458743588e4a651fff828dbd89761e04
cjw6k <cj@w6k.ca>
2024-10-20 00:35:18 -0300

    gentoo: unmask dev-haskell/parsec

--- home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install_dev_haskell-shellcheck.sh
+++ home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install_dev_haskell-shellcheck.sh
@@ -13,6 +13,7 @@ if [ ! -f /etc/portage/package.accept_keywords/dev-util/shellcheck ]; then
 
   echo '=dev-haskell/aeson-2.0.3.0 ~amd64' | sudo tee -a /etc/portage/package.accept_keywords/dev-haskell/aeson
   echo '=dev-haskell/diff-0.4.1 ~amd64' | sudo tee -a /etc/portage/package.accept_keywords/dev-haskell/diff
+  echo '=dev-haskell/parsec-3.1.15.0-r1 ~amd64' | sudo tee -a /etc/portage/package.accept_keywords/dev-haskell/parsec
 fi
 
 sudo emerge -n dev-util/shellcheck
