9e143e144a8f3e1d5eb4bb697c6da0ab14d8966f
cjw6k <cj@w6k.ca>
2024-10-20 01:02:54 -0300

    gentoo: unmask dev-haskell/regex-tdfa

--- home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install_dev_haskell-shellcheck.sh
+++ home/.chezmoiscripts/linux/gentoo/run_onchange_after_50-install_dev_haskell-shellcheck.sh
@@ -15,6 +15,7 @@ if [ ! -f /etc/portage/package.accept_keywords/dev-util/shellcheck ]; then
   echo '=dev-haskell/diff-0.4.1 ~amd64' | sudo tee -a /etc/portage/package.accept_keywords/dev-haskell/diff
   echo '=dev-haskell/parsec-3.1.15.0-r1 ~amd64' | sudo tee -a /etc/portage/package.accept_keywords/dev-haskell/parsec
   echo '=dev-haskell/quickcheck-2.14.3 ~amd64' | sudo tee -a /etc/portage/package.accept_keywords/dev-haskell/quickcheck
+  echo '=dev-haskell/regex-tdfa-1.3.2.2 ~amd64' | sudo tee -a /etc/portage/package.accept_keywords/dev-haskell/regex-tdfa
 fi
 
 sudo emerge -n dev-util/shellcheck
