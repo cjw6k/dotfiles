2cd679885f73a712f3f36350c3aa0eab1ee6c966
cjw6k <cj@w6k.ca>
2025-06-26 20:53:05 -0300

    docs: tidy up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,6 @@ version = 'latest'
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.todo',
-    # 'sphinx_rtd_dark_mode',
     'sphinxcontrib.plantuml', # https://github.com/sphinx-contrib/plantuml
 ]
 
@@ -32,11 +31,5 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-# html_theme = 'sphinx_rtd_theme'
-# html_theme_options = {
-#     'style_external_links': True,
-#     'collapse_navigation': False,
-# }
 
 html_theme = 'pyramid'
